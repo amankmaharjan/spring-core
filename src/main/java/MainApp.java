7edc40730dc6bdec70dd42cb06825d0c64806de8
aman <amanht3@gmail.com>
2017-06-12 06:01:35 +0545

    src added

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(HelloWorldConfig.class);
+        context.refresh();
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.setMessage("I'm object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+        objB.getMessage();
+    }
+}
